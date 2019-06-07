6db5125aeb61178d961fde3ce62663e84d7df995
Shaishav <s.shah19@share.epsb.ca>
2019-06-06 20:45:56 -0600

    Merge branch 'master' of C:\Users\Owner\PycharmProjects\MiniGames with conflicts.

--- ticTacToe.py
+++ ticTacToe.py
@@ -5,8 +5,18 @@ Tic-Tac-Toeeeeeee
 '''
 ### Variables ###
 
+grid = ['0','1','2','3','4','5','6','7','8','9']
+gridarray = [
+    [grid[7], '|',grid[8], '|',grid[9]],
+    ['----------'],
+    [grid[4], '|',grid[5], '|',grid[6]],
+    ['----------'],
+    [grid[1], '|',grid[2], '|',grid[3]]
+]
 repeat = True
 wins = ((7,4,1),(8,5,2),(9,6,3),(4,5,6),(7,8,9),(1,2,3),(7,5,3),(1,5,9))
+win = 0
+turn = 1
 
 ### Subroutines ###
 
@@ -17,11 +27,7 @@ def makemove(player,num):
     else:
         mark = 'O'
     #
-    if grid[num] in ['X','O']:
-        usr = int(input('Space not available, choose something else: '))
-        return makemove(player, usr)
-    else:
-        grid[num] = mark
+    grid[num] = mark
     #
 
 def printgrid():
@@ -59,7 +65,6 @@ Welcome to Tic-Tac-Toeeee !
 We hope you know how to play!
 ''')
 
-
 def main_make_move():
     global win, grid, gridarray, turn
     #
@@ -78,47 +83,34 @@ def main_make_move():
     else:
         pass
 
-def again(usr):
-    global repeat
-    usr1 = usr.lower()
-    if usr1 in ['y','yes','']:\
-        pass
+def check_if_spot_open(num): # Doesnt work idk why
+
+    global grid
+    if grid[num] == ' ' :
+        return num
     else:
-        repeat = False
+        usr = int(input('Someone is already there, choose something else: '))
+        return check_if_spot_open(usr)
+
 ###########################
 # ------ Main code ------ #
 ###########################
 start()
+printgrid()
+# Input + Processing
+while win == 0:
 
-while repeat:
-    # Initalize the variables
-    grid = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    gridarray = [
-        [grid[7], '|', grid[8], '|', grid[9]],
-        ['----------'],
-        [grid[4], '|', grid[5], '|', grid[6]],
-        ['----------'],
-        [grid[1], '|', grid[2], '|', grid[3]]
-    ]
-    win = 0
-    turn = 1
-    # First grid view
-    printgrid()
-    # Input + Processing
-    while win == 0:
+    if turn == 0:
+        main_make_move()
+    else:
+        main_make_move()
 
-        if turn == 0:
-            main_make_move()
-        else:
-            main_make_move()
-
-    # Output
-    if win == 1:
-        if turn == 1:
-            print('Player 1 won! ')
-        else:
-            print('Player 2 won! ')
-    # Again
-    again(input('Again? '))
-
+# Output
+if win == 1:
+    if turn == 1:
+        print('Player 1 won! ')
+    else:
+        print('Player 2 won! ')
 ############################
+
+
